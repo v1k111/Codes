994db62b990145edd86138b570b02a98dd31a4a2
v1k111 <mitchenkoviktor111@gmail.com>
2024-12-02 15:02:37 +0100

    12

--- bsp.c
+++ bsp.c
@@ -12,14 +12,21 @@ void generator(const int rows, const int columns, char field[rows][columns])
             field[i][j] = ' ';
         }
     }
-    const char symbols[] = {'@', '*', '^', '+'};
-    const int symbol_count = sizeof(symbols) / sizeof(symbols[0]);
+    const char symbols[] = {'@', '*', '^', '+', '>', '<', '&'};
+    const int symbol_count = columns - 2;
     int filled_columns = columns - 2;
     int used_columns[columns];
     for (int j = 0; j < filled_columns; j++)
     {
         used_columns[j] = 0;
     }
+    int s1count = 0;
+    int s2count = 0;
+    int s3count = 0;
+    int s4count = 0;
+    int s5count = 0;
+    int s6count = 0;
+    int s7count = 0;
     srand(time(NULL));
     for (int i = 0; i < filled_columns;)
     {
@@ -29,26 +36,112 @@ void generator(const int rows, const int columns, char field[rows][columns])
             used_columns[c] = 1;
             for (int v = 0; v < rows; ++v)
             {
-                field[v][c] = symbols[rand() % symbol_count];
+                char randsymbol = symbols[rand() % symbol_count];
+                if (symbol_count > 0 && randsymbol == '@' && s1count < rows)
+                {
+                    field[v][c] = randsymbol;
+                    s1count++;
+                }
+                else if (symbol_count > 1 && randsymbol == '*' && s2count < rows)
+                {
+                    field[v][c] = randsymbol;
+                    s2count++;
+                }
+                else if (symbol_count > 2 && randsymbol == '^' && s3count < rows)
+                {
+                    field[v][c] = randsymbol;
+                    s3count++;
+                }
+                else if (symbol_count > 3 && randsymbol == '+' && s4count < rows)
+                {
+                    field[v][c] = randsymbol;
+                    s4count++;
+                }
+                else if (symbol_count > 4 && randsymbol == '>' && s5count < rows)
+                {
+                    field[v][c] = randsymbol;
+                    s5count++;
+                }
+                else if (symbol_count > 5 && randsymbol == '<' && s6count < rows)
+                {
+                    field[v][c] = randsymbol;
+                    s6count++;
+                }
+                else if (symbol_count > 6 && randsymbol == '&' && s7count < rows)
+                {
+                    field[v][c] = randsymbol;
+                    s7count++;
+                }
+                else if(symbol_count < 1)
+                {
+                    return;
+                }
+                else
+                {
+                    --v;
+                }
             }
             i++;
         }
     }
 }
-void print_field(const int rows, const int columns, char field[rows][columns])
+void game_field(const int rows, const int columns, char field[rows][columns])
 {
     clear();
     start_color();
+    init_color(1, 1000, 0, 0);
+    init_color(2, 0, 1000, 0);
+    init_color(3, 0, 0, 1000);
+    init_color(4, 1000, 500, 0);
+    init_color(5, 500, 0, 1000);
+    init_color(6, 0, 1000, 1000);
+    init_color(8, 500, 500, 500);
+
+    for (int i = 1; i <= 6; i++)
+    {
+        init_pair(i, i, COLOR_BLACK);
+    }
+    init_pair(7, 8, COLOR_BLACK);
 
     for (int i = 0; i < rows; ++i)
     {
+        printw("%d ", i + 1);
+
         for (int j = 0; j < columns; ++j)
         {
-            printf("| %c ", field[i][j]);
+            printw("|");
+            attroff(COLOR_PAIR(11));
+            if (field[i][j] == '@')
+                attron(COLOR_PAIR(1));
+            else if (field[i][j] == '*')
+                attron(COLOR_PAIR(2));
+            else if (field[i][j] == '^')
+                attron(COLOR_PAIR(3));
+            else if (field[i][j] == '+')
+                attron(COLOR_PAIR(4));
+            else if (field[i][j] == '>')
+                attron(COLOR_PAIR(5));
+            else if (field[i][j] == '<')
+                attron(COLOR_PAIR(6));
+            else if (field[i][j] == '&')
+                attron(COLOR_PAIR(7));
+            printw(" %c ", field[i][j]);
+            attroff(COLOR_PAIR(1) | COLOR_PAIR(2) | COLOR_PAIR(3) | COLOR_PAIR(4) | COLOR_PAIR(5) | COLOR_PAIR(6) | COLOR_PAIR(7));
         }
-        printf("|\n");
+        printw("|\n");
     }
-    printf("\n");
+    printw("   ");
+    for (int i = 0; i < columns; i++)
+    {
+        printw("--- ");
+    }
+    printw("\n");
+    printw("   ");
+    for (int i = 0; i < columns; i++)
+    {
+        printw(" %d  ", i + 1);
+    }
+    printw("\n");
 }
 void down_possible(const int rows, const int columns, char field[rows][columns], int x, int y)
 {
@@ -140,22 +233,35 @@ bool check(const int rows, const int columns, char field[rows][columns])
 void ball_sort_puzzle()
 {
     const int rows = 4;
-    const int columns = 6;
+    const int columns = 12;
     char field[rows][columns];
     generator(rows, columns, field);
+    initscr();
+    noecho();
+    cbreak();
+
     while (!check(rows, columns, field))
     {
-        print_field(rows, columns, field);
+        game_field(rows, columns, field);
+        refresh();
         int x = -1;
         int y = -1;
-        printf("Enter what: ");
-        scanf("%d", &x);
-        printf("Enter where: ");
-        scanf("%d", &y);
+        echo();
+        printw("Enter what: ");
+        scanw("%d", &x);
+        printw("Enter where: ");
+        scanw("%d", &y);
+        noecho();
         down_possible(rows, columns, field, x, y);
+        refresh();
+
+        napms(1000);
     }
-    printf("You won!");
-    return;
+    game_field(rows, columns, field);
+    printw("You won!");
+    refresh();
+    getch();
+    endwin();
 }
 int main()
 {
